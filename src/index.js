2292b0003f83a54f2534bafc0305b1d339f0e82a
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-01-27 20:28:45 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './component/App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from'./redux/store';
+import {Provider} from "react-redux"
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
 
